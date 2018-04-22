79c065be7da0d862c484e6fad35424a043c466c4
Shaun Carroll <hello@shauncarroll.com>
2018-04-22 12:49:24 +1000

    implemented btn click handler to change background on click

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# bareBonesHtml
-Very basic template for rapid protyping
+Button click handlers in JavaScript.
+
+Clicking the button invokes the click handler which in turn changes the background to a random color that is held in an array.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
-howLong('Hello World');
+var button = document.getElementById("button");
+var rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+howLong();
 
-function howLong(foo){
+function howLong() {
   var timeStart = performance.now();
-  hello();
+  btnCheck();
   var timeEnd = performance.now();
-  console.info('%cReturned in ' + (timeEnd - timeStart) + ' ms.',"color:blue");
+  console.info('%cReturned in ' + (timeEnd - timeStart) + ' ms.', "color:blue");
 }
 
-function hello(){
-  console.log("Hello World");
+function change() {
+  document.body.style.background = rainbow[Math.floor(7 * Math.random())];
+  howLong();
+}
+
+function btnCheck() {
+  button.addEventListener("click", change);
 }
